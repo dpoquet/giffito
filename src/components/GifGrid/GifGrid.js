b83de2f34563c9abc3da909362475f3f059afd60
Dani Poquet <dani.poquet@gymforless.com>
2020-08-29 14:17:23 +0200

    Formating files

--- src/components/GifGrid/GifGrid.js
+++ src/components/GifGrid/GifGrid.js
@@ -7,10 +7,10 @@ export default function GifGrid({ gifs, isLoading }) {
 	if (isLoading) return <Loading />;
 
 	return (
-		<section className="GifGrid">
-			{gifs.map((gif) => (
-				<Gif key={gif.id} title={gif.title} url={gif.url} id={gif.id} />
-			))}
-		</section>
+    <section className="GifGrid">
+      {gifs.map((gif) => (
+        <Gif key={gif.id} title={gif.title} url={gif.url} id={gif.id} />
+      ))}
+    </section>
 	);
 }
